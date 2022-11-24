3dc6466ccf7ad8da8a44ad4f92c2744d137717e8
Aikoyori <12034280+Aikoyori@users.noreply.github.com>
2022-11-24 16:00:19 +0700

    Added some sample files so you know what you're doing p much

---
+++ components/ExampleComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-4xl">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio distinctio, nobis nemo explicabo amet vitae quibusdam vel nihil odio corporis necessitatibus ipsa, aliquam est, rerum molestiae minima. Esse, minus molestias.</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <ExampleComponent></ExampleComponent>
+</template>
+
+<script>
+export default {
+    layout (context) {
+    return 'Main'
+  }
+}
+</script>
+
+<style>
+
+</style>
